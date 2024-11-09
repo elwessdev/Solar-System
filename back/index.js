b10ed6a6bb825d598848888fe11c5167969fe3a3
Osama <benaliosama3@gmail.com>
2024-11-09 20:31:27 +0100

    setup project

---
+++ back/index.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const connectDB = require('./config/db');
+require ("dotenv").config();
+
+connectDB();
+app.use(cors({
+  origin: process.env.FRONT_LINK,
+  credentials: true,
+}));
+app.use(express.json());
+
+// Apis
+app.use("/",(req,res) => res.send("server is running"));
+app.use('/auth', require('./Routes/Auth'));
+
+// Checking
+app.listen(3001, () => {
+  console.log(`Server is running, http://localhost:3001`);
+})
