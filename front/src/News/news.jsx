04bedbd535b283d8a0b18d187bd22756a27036e9
Tabai Iyadh <tabaiiyadh317@gmail.com>
2024-11-19 00:03:16 +0100

    News Basic Style (template)

--- front/src/News/news.jsx
+++ front/src/News/news.jsx
@@ -1,9 +1,28 @@
+import React from "react";
+import "./News.scss";
+import { newsItems } from  "./data/items";
 const News = () => {
-    return(
-        <div className="news">
-            <h1>News</h1>
-        </div>
-    )
-}
+  return (
+    <div className="solar-system-news">
+      <header className="news-header">
+        <h1>Solar System News</h1>
+        <p>Explore the latest discoveries and updates about our solar system and beyond.</p>
+      </header>
+      <section className="news-list" >
+        {newsItems.map((item, index) => (
+          <div key={index} className="news-card">
+            <img src={item.image} alt={item.title} className="news-image" />
+            <div className="news-content">
+              <h2 className="news-title">{item.title}</h2>
+              <p className="news-date">{item.date}</p>
+              <p className="news-description">{item.content}</p>
+              <p className="news-viewmore"><a href={item.link}>View More</a></p>
+            </div>
+          </div>
+        ))}
+      </section>
+    </div>
+  );
+};
 
 export default News;
