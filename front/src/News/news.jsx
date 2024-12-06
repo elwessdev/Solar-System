ef98f18336e21fd41f957b9a5bb4acfaad6bd479
elwessdev <benaliosama3@gmail.com>
2024-12-06 16:13:18 +0100

    BUG - fix news api problem

--- front/src/News/news.jsx
+++ front/src/News/news.jsx
@@ -9,15 +9,15 @@ import { Link } from "react-router-dom";
 const News = () => {
   const { loginStatus } = useContext(UserContext);
   const [newsItems, setNewsItems] = useState([]);
-  const apiKey = "f1e84ae63c1f4eb181e6b2f3b694d732";
   const getNews = async () => {
     try {
-      const res = await fetch(
-        `https://newsapi.org/v2/everything?q=astronomy&apiKey=${apiKey}&sortBy=publishedAt&pageSize=20`
-      );
+      // const res = await fetch(
+      //   `https://newsapi.org/v2/everything?q=astronomy&apiKey=${import.meta.env.VITE_NEWS_KEY}&sortBy=publishedAt&pageSize=20`
+      // );
+      const res = await fetch("https://api.spaceflightnewsapi.net/v4/articles/?limit=20");
       const data = await res.json();
-      setNewsItems(data.articles);
-      console.log(data.articles);
+      setNewsItems(data.results);
+      console.log(data.results);
     } catch (error) {
       console.error("Error fetching news:", error);
     }
@@ -39,11 +39,11 @@ const News = () => {
       <section className="news-list">
         {newsItems && newsItems.slice(0,loginStatus ?newsItems.length-1 :3).map((item, index) => (
           <div key={index} className="news-card">
-            <img src={item.urlToImage} alt={item.title} className="news-image" />
+            <img src={item.image_url} alt={item.title} className="news-image" />
             <div className="news-content">
               <h2 className="news-title">{item.title}</h2>
-              <p className="news-date">{item.publishedAt}</p>
-              <p className="news-description">{item.content}</p>
+              <p className="news-date">{item.published_at}</p>
+              <p className="news-description">{item.summary}</p>
                 <a className="news-viewmore" target="_blank" href={item.url}>View More</a>
             </div>
           </div>
